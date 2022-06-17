b4022ee5e1e88d252732338e3f1533092f0b07cc
halilyurdusev <64197454+halilyurdusev@users.noreply.github.com>
2022-06-17 22:55:48 +0300

    db

--- db/conn.php
+++ db/conn.php
@@ -1,15 +1,15 @@
 <?php
-    $host = "127.0.0.1";
-    $db = "attendance_db";
-    $user = "root";
-    $pass = "";
-    $charset = "utf8mb4";
-
-    // $host = "remotemysql.com";
-    // $db = "VjZYafKgHA";
-    // $user = "VjZYafKgHA";
-    // $pass = "W2ETE9Nmsu";
+    // $host = "127.0.0.1";
+    // $db = "attendance_db";
+    // $user = "root";
+    // $pass = "";
     // $charset = "utf8mb4";
+
+    $host = "remotemysql.com";
+    $db = "VjZYafKgHA";
+    $user = "VjZYafKgHA";
+    $pass = "W2ETE9Nmsu";
+    $charset = "utf8mb4";
 
 
     $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
